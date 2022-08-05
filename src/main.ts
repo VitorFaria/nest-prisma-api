1b9ea44d443ab30667fe0c12238992e72f5e3a08
Vitor Faria <vitor.faria@ignicaodigital.com.br>
2022-08-05 18:45:05 -0300

    Config swagger doc api

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,21 @@ import { NotFoundInterceptor } from './common/errors/interceptors/notfound.inter
 import { UnauthorizedInterceptor } from './common/errors/interceptors/unauthorized.interceptor';
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Simple blog')
+    .setDescription('The simple blog API description')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
